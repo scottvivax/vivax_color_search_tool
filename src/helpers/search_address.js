b0204a427051549bfc17ac78d35a171d11145e27
bcbridges <bcb35b@gmail.com>
2022-04-27 07:49:23 -0600

    Pushing before redev on map centering

---
+++ src/helpers/search_address.js
@@ -0,0 +1,25 @@
+function search_address(map) {
+  let address = document.getElementById("address").value;
+  let state = document.getElementById("state").value;
+  let zip = document.getElementById("zip").value;
+  fetch(
+    `https://nominatim.openstreetmap.org/search?format=json&q=${address.replace(
+      " ",
+      "+"
+    )}+${state}+${zip}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      if (data.length === 0) {
+        window.alert(
+          "Address was not located, please check the entered value."
+        );
+      } else {
+        map.setView([data[0].lat, data[0].lon], 15);
+      }
+    });
+}
+
+export default search_address;
