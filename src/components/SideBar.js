cdf25d5a98fe833fb0f8664caa1b990d644ba102
Brice Bridges <bcb35b@gmail.com>
2022-05-17 07:56:29 -0600

    Fixing double tap hamburger menu on mobile issue

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,12 @@
 import AddressInput from "./AddressInput";
 import FilterInput from "./FilterInput";
 import menu_icon from "../data/list.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function SideBar({ applyFilters, map }) {
   let [collapseState, updateCollapseState] = useState("none");
 
-  function collapse_section() {
+  const collapse_section = () => {
     collapseState === "none"
       ? updateCollapseState("initial")
       : updateCollapseState("none");
@@ -18,7 +18,21 @@ function SideBar({ applyFilters, map }) {
     document
       .getElementById("filter_section")
       .setAttribute("style", `display: ${collapseState}`);
-  }
+  };
+
+  useEffect(() => {
+    if (window.innerWidth <= 1250) {
+      collapseState === "none"
+        ? updateCollapseState("initial")
+        : updateCollapseState("none");
+      document
+        .getElementById("address_section")
+        .setAttribute("style", `display: ${collapseState}`);
+      document
+        .getElementById("filter_section")
+        .setAttribute("style", `display: ${collapseState}`);
+    }
+  }, []);
 
   return (
     <div className="sidebar_container">
